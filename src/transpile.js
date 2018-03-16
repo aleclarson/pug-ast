117d3f66407e0237c7c40879dd1f8f12b424026c
aleclarson <alec.stanford.larson@gmail.com>
2018-03-15 22:30:35 -0400

    feat: transpile in parallel

--- src/transpile.js
+++ src/transpile.js
@@ -17,56 +17,74 @@ module.exports = function transpile(ast, opts) {
         return each(node.nodes, walk)
 
       case 'Code':
-        node.val = await transpile(node.val, node)
-        break
+        return transpileProp(node, 'val')
 
       case 'Conditional':
-        node.test = await transpile(node.test, node)
-        await walk(node.consequent)
-        if (node.alternate) {
-          return walk(node.alternate)
-        } else break
+        return Promise.all([
+          transpileProp(node, 'test'),
+          walk(node.consequent),
+          node.alternate && walk(node.alternate),
+        ])
 
       case 'Tag':
-        for (let attr of node.attrs)
-          attr.val = await transpile(attr.val, attr)
-        for (let block of node.attributeBlocks)
-          block.val = await transpile(block.val, block)
-        return walk(node.block)
+      case 'InterpolatedTag':
+        return Promise.all([
+          node.expr && transpileProp(node, 'expr'),
+          transpileProp(node.attrs, 'val'),
+          transpileProp(node.attributeBlocks, 'val'),
+          walk(node.block),
+        ])
 
       case 'Mixin':
         if (node.call) {
-          node.args = await transpile(node.args, node)
-          for (let attr of node.attrs)
-            attr.val = await transpile(attr.val, attr)
-          return each(node.attributeBlocks, async (node) => {
-            node.val = await transpile(node.val, node)
-          })
+          return Promise.all([
+            transpileProp(node, 'args'),
+            transpileProp(node.attrs, 'val'),
+            transpileProp(node.attributeBlocks, 'val'),
+          ])
         }
         return walk(node.block)
 
       case 'While':
-        node.test = await transpile(node.test, node)
-        return walk(node.block)
+        return Promise.all([
+          transpileProp(node, 'test'),
+          walk(node.block),
+        ])
 
       case 'Each':
-        node.obj = await transpile(node.obj, node)
-        return walk(node.block)
+        return Promise.all([
+          transpileProp(node, 'obj'),
+          walk(node.block),
+        ])
 
       case 'Case':
-        node.expr = await transpile(node.expr, node)
-        return each(node.block.nodes, async (node) => {
-          if (node.expr != 'default')
-            node.expr = await transpile(node.expr, node)
-          return walk(node.block)
-        })
+        return Promise.all([
+          transpileProp(node, 'expr'),
+          each(node.block.nodes, async (node) => {
+            return Promise.all([
+              node.expr == 'default' || transpileProp(node, 'expr'),
+              node.block && walk(node.block),
+            ])
+          })
+        ])
+    }
+  }
+
+  // Transpile the given property of every given node.
+  async function transpileProp(node, prop) {
+    if (Array.isArray(node)) {
+      await Promise.all(node.map(node => transpileProp(node, prop)))
+    } else {
+      node[prop] = await transpile(node[prop], node)
     }
   }
 }
 
-async function each(arr, fn) {
-  let i = -1, len = arr.length
-  while (++i < len) {
-    await fn(arr[i], i)
+// Parallel enumeration
+function each(arr, fn) {
+  let promises = []
+  for (let i = 0; i < arr.length; i++) {
+    promises.push(fn(arr[i], i))
   }
+  return Promise.all(promises)
 }
