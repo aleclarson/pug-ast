900e8a7e365b9c519807bfffd7d0f3126bce1c17
aleclarson <alec.stanford.larson@gmail.com>
2018-03-15 22:30:02 -0400

    simplify src/moon.js

--- src/moon.js
+++ src/moon.js
@@ -1,42 +1,25 @@
 const moonc = require('moonc')
 
+// TODO: Warn on `return` within `if|do|for|while` statements.
+
+// Pug prepends ! for `unless` and `until`.
 const negatedRE = /^!/
 
-module.exports = transpile
+// Root-level `local =` statements have `local` stripped.
+const localsRE = /(^|\n)local (?=[^=]+\s*=)/g
+
+// Root-level `return` statements have `return` stripped.
+const returnRE = /(^|\n)return /g
 
 async function transpile(code, node) {
-  try {
-    let lua = await moonc.promise(code.replace(negatedRE, 'not '))
-    if (node.type == 'Code') {
-      return stripLocals(lua)
-    }
-    // Remove the leading `return `
-    return lua.slice(7)
-  } catch(err) {
-    global.node = node
-    console.error(err)
-    return code
+  // Boolean literals may be passed for attribute values.
+  if (typeof code == 'boolean') return code
+  code = await moonc.promise(code.replace(negatedRE, 'not '))
+  code = code.toString().trim().replace(returnRE, '$1')
+  if (node.type == 'Code') {
+    code = code.replace(localsRE, '$1')
   }
+  return code
 }
 
-// Strip `local` keyword from root-level variables,
-// but only if matching `local ... =` pattern.
-function stripLocals(str) {
-  let i = 0, res = []
-  let re = /(?:^|\n)local [^=]+ =/g
-  let match
-  while (match = re.exec(str)) {
-    let j = match.index + match[0].length
-    if (i < j) {
-      res.push(str.slice(i, j))
-      i = j
-    }
-    match = match[0]
-    if (match[0] == '\n') res.push('\n')
-    res.push(match.slice(match.indexOf(' ') + 1))
-  }
-  if (i < str.length) {
-    res.push(str.slice(i))
-  }
-  return res.join('')
-}
+module.exports = transpile
